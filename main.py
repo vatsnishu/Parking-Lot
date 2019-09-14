692dc6e37e0ab2b4d36af059df46eb2ec83b5b47
nivats <nivats@microsoft.com>
2019-09-14 23:02:50 +0530

    added support for two approaches, command files as command line argument or commands as shell input

--- main.py
+++ main.py
@@ -12,37 +12,65 @@ def exceptionOccurred(exception=None):
 	exit(1)
 
 def printHelp():
-	helpString = ""
-
+	helpString = "\nPlease enter commands in following form:\ncreate_parking_lot $numberOfParkingSlots\npark $carNumber $carColor\nleave $parkingTicketNumber\nstatus\nregistration_numbers_for_cars_with_colour $color\nslot_numbers_for_cars_with_colour $color\nexit"
+	print helpString
 
 def main(argv=None):
-	parkinglot = None
-	f = open("bin/parking_lot file_inputs.txt")
-	inputsFromFile = f.readlines()
-	for inputs in inputsFromFile:
-		inputs = inputs.strip('\n')
-		command = inputs.split(' ')
-		if command[0] == "create_parking_lot" and len(command) == 2:
-			parkinglot = ParkingLot(int(command[1]))
-		elif command[0] == "park" and len(command) == 3:
-			try:
-				parkinglot.park(color=command[2], registrationNumber=command[1])
-			except Exception as e:
-				exceptionOccurred(exception=e)
-		elif command[0] == "status" and len(command) == 1:
-			parkinglot.status()
-		elif command[0] == "leave" and len(command) == 2:
-			parkinglot.leave(int(command[1]))
-		elif command[0] == "slot_numbers_for_cars_with_colour" and len(command) == 2:
-			parkinglot.slotNumbersOfCarWithGivenColour(command[1])
-		elif command[0] == "registration_numbers_for_cars_with_colour" and len(command) == 2:
-			parkinglot.registrationNumbersOfCarWithGivenColour(command[1])
-		elif command[0] == "slot_number_for_registration_number" and len(command) == 2:
-			parkinglot.slotNumberCarWithGivenRegistrationNumber(command[1])
-		elif command[0] == "exit" and len(command) == 1:
-			exit(0)
-		else:
-			printHelp()
+	# takes input from command line
+	if len(sys.argv) == 1:
+		while(1):
+			inputs = raw_input()
+			command = inputs.split(' ')
+			if command[0] == "create_parking_lot" and len(command) == 2:
+				parkinglot = ParkingLot(int(command[1]))
+			elif command[0] == "park" and len(command) == 3:
+				try:
+					parkinglot.park(color=command[2], registrationNumber=command[1])
+				except Exception as e:
+					exceptionOccurred(exception=e)
+			elif command[0] == "status" and len(command) == 1:
+				parkinglot.status()
+			elif command[0] == "leave" and len(command) == 2:
+				parkinglot.leave(int(command[1]))
+			elif command[0] == "slot_numbers_for_cars_with_colour" and len(command) == 2:
+				parkinglot.slotNumbersOfCarWithGivenColour(command[1])
+			elif command[0] == "registration_numbers_for_cars_with_colour" and len(command) == 2:
+				parkinglot.registrationNumbersOfCarWithGivenColour(command[1])
+			elif command[0] == "slot_number_for_registration_number" and len(command) == 2:
+				parkinglot.slotNumberCarWithGivenRegistrationNumber(command[1])
+			elif command[0] == "exit" and len(command) == 1:
+				exit(0)
+			else:
+				printHelp()
+	#takes input from file
+	else:
+		parkinglot = None
+		f = open(sys.argv[1])
+		inputsFromFile = f.readlines()
+		for inputs in inputsFromFile:
+			inputs = inputs.strip('\n')
+			command = inputs.split(' ')
+			if command[0] == "create_parking_lot" and len(command) == 2:
+				parkinglot = ParkingLot(int(command[1]))
+			elif command[0] == "park" and len(command) == 3:
+				try:
+					parkinglot.park(color=command[2], registrationNumber=command[1])
+				except Exception as e:
+					exceptionOccurred(exception=e)
+			elif command[0] == "status" and len(command) == 1:
+				parkinglot.status()
+			elif command[0] == "leave" and len(command) == 2:
+				parkinglot.leave(int(command[1]))
+			elif command[0] == "slot_numbers_for_cars_with_colour" and len(command) == 2:
+				parkinglot.slotNumbersOfCarWithGivenColour(command[1])
+			elif command[0] == "registration_numbers_for_cars_with_colour" and len(command) == 2:
+				parkinglot.registrationNumbersOfCarWithGivenColour(command[1])
+			elif command[0] == "slot_number_for_registration_number" and len(command) == 2:
+				parkinglot.slotNumberCarWithGivenRegistrationNumber(command[1])
+			elif command[0] == "exit" and len(command) == 1:
+				exit(0)
+			else:
+				printHelp()
 
 
 
